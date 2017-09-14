c704120606731681a718d1efe389e3f128cabb66
Azmi ŞAHİN <azmisahin@outlook.com>
2017-09-14 20:29:02 +0300

    Add a istanbul Code Coverage

---
+++ test/tests.js
@@ -0,0 +1,30 @@
+/**
+ * ====================================================================================================
+ * Name    : Tests Global
+ * File    : tests.js
+ * Version : 0.0.1
+ * ====================================================================================================
+ */
+
+// Global Requared
+var assert = require("assert");
+var chai = require('chai');
+var expect = chai.expect;
+
+// Test Object
+
+'use strict';
+
+/**
+ * Test Iteration
+ * 
+ */
+module.exports = function () {
+    it("Iteration Global Test", function () {
+
+        // Define
+        console.log("Global Test Iteration");
+
+        // Expect
+    });
+}
